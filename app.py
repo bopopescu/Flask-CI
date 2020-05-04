b77a82c95ac9fd36839ddf070a1754f9a46d173d
PJprogrammer <pauljprogrammer@gmail.com>
2020-05-04 16:17:30 -0400

    Dummy endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
@@ -7,9 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Server is working!'
 
-@app.route('/')
-def hello_world():
-    return 'Server is working!'
+
+@app.route('/data')
+def data():
+    return make_response(jsonify({"response": "Successful"}), 200)
 
 if __name__ == '__main__':
     app.run()
